39e3785dd01c0ac87ba69f328d4a7dd3da8ebce1
ManojGowda2006 <manojgowdan807@gmail.com>
2025-04-08 11:46:33 +0530

    done

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+const jwt = require('jsonwebtoken')
+
 const encrypt = (payload) => {
   // encrypt the payload and return token
+  const token = jwt.sign({
+    data : payload},
+    'secret',
+    {expiresIn : 60 * 60}
+  )
+  console.log("Token : ",token);
+  return token;
 }
 
 const decrypt = (token) => {
   // return decoded payload
+  const decoded = jwt.verify(token,'secret')
+  console.log("Decoded token : ",decoded)
+  return decoded;
 }
 
 module.exports = {
